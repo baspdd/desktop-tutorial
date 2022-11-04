c98c655baa5f6ebcb462e0f5bb175f50098333b1
baspdd <duypdhe160308@fpt.edu.vn>
2022-11-04 23:28:22 +0700

    up

--- PE_PRN211_Student/Q2/Controllers/HomeController.cs
+++ PE_PRN211_Student/Q2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Q2.Controllers
 {
@@ -6,6 +7,21 @@ namespace Q2.Controllers
     {
         public IActionResult Index()
         {
+            DataProvider data = new DataProvider();
+            IDataReader dr = data.executeQuery2("SELECT [ProductID]\r\n      ,[ProductName]\r\n  FROM [dbo].[Products]");
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[2] { new DataColumn("ProductID"), new DataColumn("ProductName")});
+            
+            List<int> list = new List<int>();
+
+            while (dr.Read())
+            {
+                dt.Rows.Add(dr.GetInt32(0), dr.GetString(1));
+                list.Add(dr.GetInt32(0));
+            }
+            dr.Close();
+            
+            ViewBag.ListP = list;
             return View();
         }
 
